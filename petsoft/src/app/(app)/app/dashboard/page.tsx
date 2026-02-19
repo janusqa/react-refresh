b4e9bb1f929f25df0d540ce5935a8e69ebf25970
Janus Developer <dev@retrievo.net>
2026-02-19 09:17:27 -0400

    add branding and stats to dashboard

--- petsoft/src/app/(app)/app/dashboard/page.tsx
+++ petsoft/src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import Branding from '@/components/branding';
+import Stats from '@/components/stats';
+
 export default function Page() {
-    return <div>Dashboard</div>;
+    return (
+        <main>
+            <div className="flex flex-row justify-between items-center text-white py-8">
+                <Branding />
+                <Stats />
+            </div>
+        </main>
+    );
 }
