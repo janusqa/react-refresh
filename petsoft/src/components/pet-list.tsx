b6cee5619569567254b61d6d78de122d8d5176a7
Janus Developer <dev@retrievo.net>
2026-02-19 11:31:50 -0400

    build out dashboard by adding grid layout search, pet list, pet details

---
+++ petsoft/src/components/pet-list.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+export default function PetList() {
+    return (
+        <ul className="bg-white border-b border-black/8">
+            <li>
+                <button className="flex flex-row items-center h-17.5 w-full px-5 text-base gap-3 hover:bg-[#EFF1F1] focus:bg-[#EFF1F1] transition">
+                    <Image
+                        src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+                        alt="Pet image"
+                        width={45}
+                        height={45}
+                        className="rounded-full object-cover"
+                    />
+                    <p className="font-semibold">Benjamin</p>
+                </button>
+            </li>
+            <li>
+                <button className="flex flex-row items-center h-17.5 w-full px-5 text-base gap-3 hover:bg-[#EFF1F1] focus:bg-[#EFF1F1] transition">
+                    <Image
+                        src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+                        alt="Pet image"
+                        width={45}
+                        height={45}
+                        className="rounded-full object-cover"
+                    />
+                    <p className="font-semibold">Benjamin</p>
+                </button>
+            </li>
+        </ul>
+    );
+}
